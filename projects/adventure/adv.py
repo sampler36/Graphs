cf6f0d04c59b730712399d04fe8f6406f729e213
Talent Antonio <antonio.talenty@gmail.com>
2019-06-20 13:50:27 +0100

    Adding Que class / adv.py

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -21,6 +21,18 @@ world.printRooms()
 
 player = Player("Name", world.startingRoom)
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 # Fill this out
 traversalPath = []
 
